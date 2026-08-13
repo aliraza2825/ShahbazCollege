652f6f6d2aa1bcded7527d84bce261989dcb110c
araza4751 <ali.trodosoft@gmail.com>
2026-08-13 20:11:07 +0500

    push

--- controllers/Api.php
+++ controllers/Api.php
@@ -3462,7 +3462,7 @@ class Api extends CI_Controller {
 		$type = $this->input->post('type');
 
 		$student_details = $this->db->get_where('students',array('student_id'=>$student_id))->result_array();
-        print_r($student_details);
+        print_r($student_id);
         exit();
 		$my_result=array();
 		if($type=='board')
